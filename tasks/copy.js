d5f23fc692d6b47b3c167d0eb6ffac1a0c39e718
Kristopher Wilson <kristopherwilson@gmail.com>
2015-05-21 05:13:39 -0400

    Speed up copy by not reading the files

--- tasks/copy.js
+++ tasks/copy.js
@@ -5,11 +5,11 @@ module.exports = function (gulp, config) {
     return gulp.src([
       'node_modules/bootstrap-sass/assets/fonts/bootstrap/*.{ttf,woff,woff2,svg,eot}',
       'node_modules/uniform/assets/fonts/*'
-    ]).pipe(gulp.dest(config.dist.root + '/fonts'));
+    ], {read:false}).pipe(gulp.dest(config.dist.root + '/fonts'));
   });
 
   gulp.task('copy-images', ['clean'], function () {
-    return gulp.src(config.src.root + '/img/*.{png,gif,jpg}')
+    return gulp.src(config.src.root + '/img/*.{png,gif,jpg}', {read:false})
       .pipe(gulp.dest(config.dist.root + '/img'));
   });
 
